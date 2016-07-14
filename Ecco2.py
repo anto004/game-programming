cbeb530b097337493b41823fd5e7ec4e06a82889
Antonio Bang <anto_004@ymail.com>
2016-07-13 23:29:38 -0700

    added obstacles planets

--- Ecco2.py
+++ Ecco2.py
@@ -37,8 +37,8 @@ def addInstructions(pos, msg):
                         pos=(-1.3, pos), align=TextNode.ALeft, scale=.05)
 
 def addCoinScore(score):
-    return OnscreenText(text=score, style=1, fg=(1, 1, 1, 1),
-                        pos=(-1.3, 10), align=TextNode.ALeft, scale=.05)
+    return OnscreenText(text="Coins: ", style=1, fg=(1, 1, 1, 1),
+                        pos=(0, 0.95), align=TextNode.ALeft, scale=.05)
 
 class EccoGame(ShowBase):
 
@@ -50,18 +50,20 @@ class EccoGame(ShowBase):
         self.setupFloor()
         self.setupCharacter()
 
-        self.title = addTitle("Panda3D - Ecco ")
+        #self.title = addTitle(" ")
         self.inst1 = addInstructions(0.95, "[ESC]: Quit")
         self.inst2 = addInstructions(0.90, "[Left key]: Turn Ecco Left")
         self.inst3 = addInstructions(0.85, "[Right key]: Turn Ecco Right")
-        self.inst4 = addInstructions(0.80, "[Space]: Jump Ecco")
+        self.inst4 = addInstructions(0.80, "[Up key]: Jump Ecco")
+        self.inst5 = addCoinScore("score")
 
         inputState.watchWithModifiers('esc', 'escape')
-        inputState.watchWithModifiers('arrow_up', 'w')
+        inputState.watchWithModifiers('w', 'w')
         inputState.watchWithModifiers('arrow_left', 'arrow_left')
         inputState.watchWithModifiers('arrow_right', 'arrow_right')
         inputState.watchWithModifiers('pause', 'p')
         inputState.watchWithModifiers('space', 'space')
+        inputState.watchWithModifiers('arrow_up', 'arrow_up')
 
         inputState.watchWithModifiers('cam-left', 'z')
         inputState.watchWithModifiers('cam-right', 'x')
@@ -87,28 +89,42 @@ class EccoGame(ShowBase):
 
         # Set up Coins as Collectables
         self.setupCoins()
+
+        #Set up Obstacles
+        self.setupObstacles()
+
         self.coinsCollected = 0
+        self.dictOfCoins = {}
 
     def update(self, task):
         self.setupSky()
-        #self.setUpCamera()
+        self.setUpCamera()
         self.processInput()
         self.processContacts()
+        self.checkIfEccoDied()
         dt = globalClock.getDt()
-        self.world.doPhysics(dt, 10, 1 / 180.0)
+        #self.world.doPhysics(dt, 10, 1 / 180.0)
+        self.world.doPhysics(dt, 10, 1 / 230.0)
         return task.cont
 
     def setupWorld(self):
         # create bullet world
         self.debugNP = self.render.attachNewNode(BulletDebugNode('Debug'))
-        self.debugNP.show()
+        #self.debugNP.show()
 
         self.world = BulletWorld()
-        self.world.setGravity(Vec3(0, 0, -900.81))
+        self.world.setGravity(Vec3(0, 0, -9.81))
         self.world.setDebugNode(self.debugNP.node())
 
     def setupSky(self):
+
+        self.milkyWayNp = render.attachNewNode('milkyway')
+        self.milkyWay_2Np = render.attachNewNode('milkyway_2')
+        self.marsNp = render.attachNewNode('mars')
+        self.sunNp = render.attachNewNode('sun')
+
         # Load the model for the sky
+        #self.sky = loader.loadModel("models/sky/solar_sky_sphere")
         self.sky = loader.loadModel("models/sky/solar_sky_sphere")
         # Load the texture for the sky.
         self.sky_tex = loader.loadTexture("models/sky/stars_1k_tex.jpg")
@@ -117,28 +133,64 @@ class EccoGame(ShowBase):
         # Parent the sky model to the render node so that the sky is rendered
         self.sky.reparentTo(self.render)
         # Scale the size of the sky.
-        self.sky.setScale(40000)
-        self.sky.setPos(self.characterNP.getX(), self.characterNP.getY() + 10000, 0)
+        self.sky.setScale(15000)
+        x = 0.005
+        y = 1700.0
+        z = 0.0
+        self.sky.setPos(x, y, 0)
+
+        # #milkyway 1
+        self.milkyWay = loader.loadModel("models/sky/planet_sphere")
+        self.milkWay_tex = loader.loadTexture("models/sky/milkyway_tex.jpg")
+        self.milkyWay.setTexture(self.milkWay_tex, 1)
+        self.milkyWay.reparentTo(self.milkyWayNp)
+        self.milkyWay.setScale(200)
+        self.milkyWay.setPos(x + 2000, y + 10000, z - 500)
+        #
+        # #milkyway 2
+        self.milkyWay_2 = loader.loadModel("models/sky/planet_sphere")
+        self.milkWay_2_tex = loader.loadTexture("models/sky/milkyway_2_tex.jpg")
+        self.milkyWay_2.setTexture(self.milkWay_2_tex, 1)
+        self.milkyWay_2.reparentTo(self.milkyWay_2Np)
+        self.milkyWay_2.setScale(400)
+        self.milkyWay_2.setPos(x - 3000, y + 10000, z + 500)
+
+        # #sun
+        self.sun = loader.loadModel("models/sky/planet_sphere")
+        self.sun_tex = loader.loadTexture("models/sky/sun_2_tex.jpg")
+        self.sun.setTexture(self.sun_tex, 1)
+        self.sun.reparentTo(self.sunNp)
+        self.sun.setScale(600)
+        self.sun.setPos(x + 1000, y + 10000, z + 1000)
+        #
+        # # Load Mars
+        self.mars = loader.loadModel("models/sky/planet_sphere")
+        self.mars_tex = loader.loadTexture("models/sky/mars_1k_tex.jpg")
+        self.mars.setTexture(self.mars_tex, 1)
+        self.mars.reparentTo(self.marsNp)
+        self.mars.setScale(200)
+        self.mars.setPos(x + 3000, y + 10000, z + 500)
+
 
     def setupSound(self):
         # Set up sound
         mySound = base.loader.loadSfx("sounds/Farm Morning.ogg")
-        self.footsteps = base.loader.loadSfx("sounds/Footsteps_on_Cement-Tim_Fryer-870410055.ogg")
+        #self.footsteps = base.loader.loadSfx("sounds/Footsteps_on_Cement-Tim_Fryer.wav")
         mySound.play()
-        mySound.setVolume(1.5)
+        mySound.setVolume(3.0)
         mySound.setLoop(True)
 
     def setupFloor(self):
         # Floor
-        shape = BulletPlaneShape(Vec3(0, 0, 1), 0)
-        floorNP = self.render.attachNewNode(BulletRigidBodyNode('Floor'))
-        floorNP.node().addShape(shape)
-        floorNP.setPos(0, 0, -300)
-        floorNP.setCollideMask(BitMask32.allOn())
-        self.world.attachRigidBody(floorNP.node())
+        # shape = BulletPlaneShape(Vec3(0, 0, 1), 0)
+        # floorNP = self.render.attachNewNode(BulletRigidBodyNode('Floor'))
+        # floorNP.node().addShape(shape)
+        # floorNP.setPos(0, 0, -300)
+        # floorNP.setCollideMask(BitMask32.allOn())
+        # self.world.attachRigidBody(floorNP.node())
 
         #origin = Point3(2, 0, 0)
-        size = Vec3(10, 10000, 1)
+        size = Vec3(10, 5000, 1)
         shape = BulletBoxShape(size * 0.55)
         stairNP = self.render.attachNewNode(BulletRigidBodyNode('Floor'))
         stairNP.node().addShape(shape)
@@ -184,9 +236,6 @@ class EccoGame(ShowBase):
         camvec = self.characterNP.getPos() - self.camera.getPos()
         camvec.setZ(0)
         camdist = camvec.length()
-        print self.characterNP.getPos()
-        print "and"
-        print camdist
         camvec.normalize()
         if camdist > 10.0:
             self.camera.setPos(self.camera.getPos() + camvec * (camdist - 60))
@@ -200,46 +249,46 @@ class EccoGame(ShowBase):
     def processInput(self):
         dt = globalClock.getDt()
         startpos = self.ecco.getPos()
-
         speed = Vec3(0, 0, 0)
         omega = 0.0
-
         if inputState.isSet('esc'): sys.exit()
-        if inputState.isSet('arrow_up'): speed.setY(20.0)
+        if inputState.isSet('w'): speed.setY(20.0)
         if inputState.isSet('arrow_left'):
             speed.setX(-35.0)
             #omega = 120.0
         if inputState.isSet('arrow_right'):
             speed.setX(35.0)
             #omega = -120.0
-        if inputState.isSet('space'):
-            self.jump()
-
+        if inputState.isSet('space'): self.jump()
+        if inputState.isSet('arrow_up'): self.jump()
         if inputState.isSet('cam-left'): self.camera.setX(self.camera, -20 * dt)
         if inputState.isSet('cam-right'): self.camera.setX(self.camera, +20 * dt)
         if inputState.isSet('cam-forward'): self.camera.setY(self.camera, -200 * dt)
         if inputState.isSet('cam-backward'): self.camera.setY(self.camera, +200 * dt)
 
         #Make Ecco run
-        # if self.isMoving is False:
-        #     self.ecco.loop("run")
-        #     self.isMoving = True
-        # speed.setY(50.0)
+        if self.isMoving is False:
+            self.ecco.loop("run")
+            self.isMoving = True
+        speed.setY(20.0)
+
         # self.footsteps.play()
-        # self.footsteps.setVolume(10)
+        # self.footsteps.setVolume(150)
         #self.character.setAngularMovement(omega)
         self.character.setLinearMovement(speed, True)
 
     def jump(self):
         self.character.setMaxJumpHeight(5.0)
         self.character.setJumpSpeed(10.0)
-        self.character.doJump()
+        if self.character.isOnGround():
+            self.character.doJump()
+
 
     def setupCoins(self):
         self.coins = []
-        for i in range(5):
-            randX = random.uniform(0, 3.5)
-            randY = random.randint(0, 50)
+        for i in range(10):
+            randX = random.uniform(-3.5, 3.5)
+            randY = random.randint(0, 1000)
             shape = BulletSphereShape(0.75)
             coinNode = BulletGhostNode('Coin-' + str(i))
             coinNode.addShape(shape)
@@ -249,8 +298,10 @@ class EccoGame(ShowBase):
 
             #Adding sphere model
             sphereNp = loader.loadModel('models/smiley.egg')
+            sphereNp_tex = loader.loadTexture("models/sky/coin_2_tex.jpg")
+            sphereNp.setTexture(sphereNp_tex, 1)
             sphereNp.reparentTo(np)
-            sphereNp.setScale(1)
+            sphereNp.setScale(0.85)
 
             self.world.attachGhost(coinNode)
             self.coins.append(coinNode)
@@ -260,10 +311,13 @@ class EccoGame(ShowBase):
         # self.testWithEveryBody()
 
         for coin in self.coins:
-            numOfContacts = self.testWithSingleBody(coin)
-            if numOfContacts == 1:
-                self.coinsCollected += 1
-                print "coins Collected:" + str(self.coinsCollected)
+            self.testWithSingleBody(coin)
+            for key in self.dictOfCoins:
+                x = set(key)
+                #print "length of x" + str(len(x) - 1)
+                self.coinsCollected = len(x) - 1
+        #self.CoinsScore = addCoinScore(str(self.coinsCollected))
+        #print self.coinsCollected
 
     def testWithSingleBody(self, secondNode):
         # test sphere for contacts with secondNode
@@ -275,15 +329,63 @@ class EccoGame(ShowBase):
                 node0 = contact.getNode0()
                 node1 = contact.getNode1()
                 print node0.getName(), node1.getName()
+                self.dictOfCoins[node1.getName()] = 1
                 print "Removing:" + str(node1.getName())
                 np = self.render.find(node1.getName())
                 np.node().removeAllChildren()
                 self.world.removeGhost(np.node())
-        return contactResult.getNumContacts()
 
-            #self.world.removeGhost(node1)
-        # if len(contactResult.getContacts()) > 0:
-        #     print "Ecco is in contact with: ", secondNode.getName()
+    def setupObstacles(self):
+
+        # Obstacle
+        origin = Point3(2, 0, 0)
+        size = Vec3(3, 4.75, 3.5)
+        shape = BulletBoxShape(size * 0.55)
+        for i in range(3):
+            randX = random.uniform(-3.5, 3.5)
+            randY = random.randint(0, 1000)
+            pos = origin + size * i
+            #pos.setY(0)
+            ObstacleNP = self.render.attachNewNode(BulletRigidBodyNode('Obstacle%i' % i))
+            ObstacleNP.node().addShape(shape)
+            #ObstacleNP.setPos(randX, randY, 2)
+            ObstacleNP.setPos(randX, randY, 2)
+            ObstacleNP.setCollideMask(BitMask32.allOn())
+
+            modelNP = loader.loadModel('models/box.egg')
+            modelNP_tex = loader.loadTexture("models/sky/milkyway_tex.jpg")
+            modelNP.setTexture(modelNP_tex, 1)
+            modelNP.reparentTo(ObstacleNP)
+            # modelNP.setPos(0, 0, 0)
+            modelNP.setPos(-size.x / 2.0, -size.y / 2.0, -size.z / 2.0)
+            modelNP.setScale(size)
+            self.world.attachRigidBody(ObstacleNP.node())
+
+        size_2 = Vec3(3, 4.75, 1.5)
+        for i in range(3):
+            randX = random.uniform(-3.5, 3.5)
+            randY = random.randint(0, 1000)
+            pos = origin + size_2 * i
+            pos.setY(0)
+            ObstacleNP = self.render.attachNewNode(BulletRigidBodyNode('Obstacle%i' % i))
+            ObstacleNP.node().addShape(shape)
+            # ObstacleNP.setPos(randX, randY, 2)
+            ObstacleNP.setPos(randX, randY, 2)
+            ObstacleNP.setCollideMask(BitMask32.allOn())
+
+            modelNP = loader.loadModel('models/box.egg')
+            modelNP_tex = loader.loadTexture("models/sky/moon_1k_tex.jpg")
+            modelNP.setTexture(modelNP_tex, 1)
+            modelNP.reparentTo(ObstacleNP)
+            # modelNP.setPos(0, 0, 0)
+            modelNP.setPos(-size_2.x / 2.0, -size_2.y / 2.0, -size_2.z / 2.0)
+            modelNP.setScale(size_2)
+            self.world.attachRigidBody(ObstacleNP.node())
+
+    def checkIfEccoDied(self):
+        pos = self.characterNP.getPos()
+        if pos.getZ() < -50.0:
+            sys.exit(1)
 
 
 simulation = EccoGame()
